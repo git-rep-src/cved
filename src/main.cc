c2d3f5737fb989838361c25cce8824d1aa9e3096
git-rep <git-rep@protonmail.com>
2018-12-23 15:57:14 -0300

    Add argument parser

--- src/main.cc
+++ src/main.cc
@@ -2,9 +2,25 @@
 
 #include <QApplication>
 #include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    if (argc == 2) {
+        if ((strcmp("-h", argv[1]) == 0) || (strcmp("--help", argv[1]) == 0)) {
+            qDebug() << "CVE's Docker containers manager" << endl;
+            qDebug() << "Usage: cved [OPTION]" << endl;
+            qDebug() << "Options:";
+            qDebug() << "  -n, --no-update no update database";
+            qDebug() << "  -h, --help      display this help and exit";
+            qDebug() << "  -v, --version   output version information and exit";
+            return 0;
+        } else if ((strcmp("-v", argv[1]) == 0) || (strcmp("--version", argv[1]) == 0)) {
+            qDebug() << "0.1.0";
+            return 0;
+        }
+    }
+
     QApplication application(argc, argv);
     
     QFile file(":/style");
